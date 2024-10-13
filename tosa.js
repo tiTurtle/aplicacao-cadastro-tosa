44cd2f421e673b31a6771b7dd3be2f0dcb738ccc
tiTurtle <matheus.srodrigues@al.infnet.edu.br>
2024-10-13 00:54:33 -0300

    Criando classes e funções para puxar os elementos da tela

--- tosa.js
+++ tosa.js
@@ -1,2 +1,54 @@
-const outro = document.getElementById('tipo').value;
+//Classes
+class Pet {
+    constructor(senha, nome, preco) {
+      this.senha = senha;
+      this.nome = nome;
+      this.preco = preco;
+    }
+    toString() {
+      return `${this.senha} - ${this.nome} - R$${this.preco.toFixed(2)}`;
+    }
+  }
+  
+  class Tutor {
+    constructor(nome, celular, email) {
+      this.nome = nome;
+      this.celular = celular;
+      this.email = email;
+    }
+    toString() {
+      return `${this.nome} = ${this.celular}`;
+    }
+  }
+  
+  class Tosa {
+    constructor(senha, tutor) {
+      this.senha = senha;
+      this.tutor = tutor;
+      this.pets = [];
+      this.status = "Na Fila";
+    }
 
+  }
+
+  function pegarDadosTosa () {
+    const nome =  document.getElementById("nome").value;
+    const celular = document.getElementById("celular").value;
+    const email = document.getElementById("email").value;
+    const senha = Math.floor(1000 + Math.random() * 9000);
+
+    const tutor = new Tutor (nome, celular, email)
+
+    const tosa = new Tosa(tutor, senha)
+    return tosa;
+};
+
+function pegarDadosPet () {
+    const nome = document.getElementById("nomePet").value;
+    const tipo = document.getElementById("tipo").value
+    const preco = document.getElementById("preco")
+
+    const pet = new Pet(nome, tipo, preco );
+
+    return pet
+}
